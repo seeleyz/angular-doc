00bed14d0b4c483fb7bcdb6a901c0df27e57b55d
张丽 <1344906651@qq.com>
2018-07-27 17:08:34 +0800

    dd

--- src/app/template-syntax/template-syntax.module.ts
+++ src/app/template-syntax/template-syntax.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 
-import { HomeComponent } from './home/home.component';
-
 import { TemplateSyntaxRouteModule } from './template-syntax.route';
+import { HomeComponent } from './home/home.component';
+import { TemRefVarComponent } from './tem-ref-var/tem-ref-var.component';
+import { BuiltInStrucDirectiveComponent } from './built-in-struc-directive/built-in-struc-directive.component';
 
 @NgModule({
   imports: [
@@ -13,7 +14,9 @@ import { TemplateSyntaxRouteModule } from './template-syntax.route';
     TemplateSyntaxRouteModule
   ],
   declarations: [
-    HomeComponent
+    HomeComponent,
+    TemRefVarComponent,
+    BuiltInStrucDirectiveComponent
   ]
 })
 export class TemplateSyntaxModule { }
